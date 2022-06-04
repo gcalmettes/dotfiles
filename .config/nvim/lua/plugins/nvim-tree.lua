f61587ffb459d92c2e795a800bf7522831158a0b
Guillaume Calmettes <gcalmettes@fieldbox.ai>
2022-06-04 10:43:33 +0200

    [FEAT] update to latest plugins version - nvimTree and GitDiffView

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -2,68 +2,10 @@ local g = vim.g
 local cmd = vim.cmd
 utils = require('utils')
 
-
-
 -- These additional options must be set BEFORE calling require'nvim-tree' or calling setup.
 -- They are being migrated to the setup function bit by bit,
-g.nvim_tree_git_hl = 0 -- this will enable file highlight for git attributes (can be used without the icons).
-g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
-g.nvim_tree_root_folder_modifier = ':~' -- This is the default. See :help filename-modifiers for more options
-g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
-g.nvim_tree_group_empty = 1 -- compact folders that only contain a single folder into one node in the file tree
-g.nvim_tree_icon_padding = ' ' -- one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
-g.nvim_tree_symlink_arrow = ' >> ' -- defaults to ' ➛ '. used as a separator between symlinks' source and target.
 -- g.nvim_tree_respect_buf_cwd = 1 -- 0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
-g.nvim_tree_create_in_closed_folder = 0 -- 1 by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
 g.nvim_tree_refresh_wait = 500 -- 1000 by default, control how often the tree can be refreshed, 1000 means the tree can be refresh once per 1000ms.
-
-
-
--- List of filenames that gets highlighted with NvimTreeSpecialFile
-g.nvim_tree_special_files = { 
-  ['README.md'] = 1,
-  ['Makefile'] = 1,
-  ['MAKEFILE'] = 1,
-}
-
--- If 0, do not show the icons for one of 'git' 'folder' and 'files'
--- 1 by default, notice that if 'files' is 1, it will only display
--- if nvim-web-devicons is installed and on your runtimepath.
--- if folder is 1, you can also tell folder_arrows 1 to show small arrows next to the folder icons.
--- but this will not work when you set indent_markers (because of UI conflict)
-g.nvim_tree_show_icons = {
-  ['git'] = 1,
-  ['folders'] = 1,
-  ['files'] = 1,
-  ['folder_arrows'] = 0,
-}
-
--- default will show icon by default if no icon is provided
--- default shows no icon by default
-g.nvim_tree_icons = {
-  ['default'] = '',
-  ['symlink'] = '',
-  ['git'] = {
-    ['unstaged'] = "✗",
-    ['staged'] = "✓",
-    ['unmerged'] = "",
-    ['renamed'] = "➜",
-    ['untracked'] = "★",
-    ['deleted'] = "",
-    ['ignored'] = "◌"
-  },
-  ['folder']= {
-    ['arrow_open'] = "⬎",
-    ['arrow_closed'] = "⬏",
-    ['default'] = "",
-    ['open'] = "",
-    ['empty'] = "",
-    ['empty_open'] = "",
-    ['symlink'] = "",
-    ['symlink_open'] = "",
-  }
- }
-
 
 
 require('nvim-tree').setup {
@@ -77,6 +19,7 @@ require('nvim-tree').setup {
   git = {
     ignore = false,
   },
+  create_in_closed_folder = false, -- true by default, When creating files, sets the path of a file when cursor is on a closed folder to the parent folder when 0, and inside the folder when 1.
   update_to_buf_dir   = {
     enable = true, -- this option allows the cursor to be updated when entering a buffer
     auto_open = false,
@@ -115,6 +58,11 @@ require('nvim-tree').setup {
     }
   },
   renderer = {
+    group_empty = true, -- compact folders that only contain a single folder into one node in the file tree
+    highlight_opened_files = "icon", -- "none" by default, will enable folder and file icon highlight for opened files/directories.
+    root_folder_modifier = ':~', -- This is the default. See :help filename-modifiers for more options
+    add_trailing = false, -- append a trailing slash to folder names
+    highlight_git = false, -- this will enable file highlight for git attributes (can be used without the icons).
     indent_markers = {
       enable = true,
       icons = {
@@ -123,6 +71,52 @@ require('nvim-tree').setup {
         none = "  ",
       },
     },
+    -- List of filenames that gets highlighted with NvimTreeSpecialFile
+    special_files = { 
+      ['README.md'] = 1,
+      ['Makefile'] = 1,
+      ['MAKEFILE'] = 1,
+    },
+    icons = {
+      padding = ' ', -- one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
+      symlink_arrow = ' >> ', -- defaults to ' ➛ '. used as a separator between symlinks' source and target.
+      -- If 0, do not show the icons for one of 'git' 'folder' and 'files'
+      -- 1 by default, notice that if 'files' is 1, it will only display
+      -- if nvim-web-devicons is installed and on your runtimepath.
+      -- if folder is 1, you can also tell folder_arrows 1 to show small arrows next to the folder icons.
+      -- but this will not work when you set indent_markers (because of UI conflict)
+      show = {
+        ['git'] = true,
+        ['folder'] = true,
+        ['file'] = true,
+        ['folder_arrow'] = false,
+      },
+      glyphs = {
+        -- default will show icon by default if no icon is provided
+        -- default shows no icon by default
+        ['default'] = '',
+        ['symlink'] = '',
+        ['git'] = {
+          ['unstaged'] = "✗",
+          ['staged'] = "✓",
+          ['unmerged'] = "",
+          ['renamed'] = "➜",
+          ['untracked'] = "★",
+          ['deleted'] = "",
+          ['ignored'] = "◌"
+        },
+        ['folder']= {
+          ['arrow_open'] = "⬎",
+          ['arrow_closed'] = "⬏",
+          ['default'] = "",
+          ['open'] = "",
+          ['empty'] = "",
+          ['empty_open'] = "",
+          ['symlink'] = "",
+          ['symlink_open'] = "",
+        }
+      }
+    }
   },
   actions = {
     -- use_system_clipboard = true,
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -166,14 +166,22 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: diffview.nvim
-time([[Config for diffview.nvim]], true)
-try_loadstring("\27LJ\2\n3\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\24plugins.gitdiffview\frequire\0", "config", "diffview.nvim")
-time([[Config for diffview.nvim]], false)
+-- Config for: nvim-lspconfig
+time([[Config for nvim-lspconfig]], true)
+try_loadstring("\27LJ\2\n+\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\16plugins.lsp\frequire\0", "config", "nvim-lspconfig")
+time([[Config for nvim-lspconfig]], false)
 -- Config for: nvim-compe
 time([[Config for nvim-compe]], true)
 try_loadstring("\27LJ\2\n-\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\18plugins.compe\frequire\0", "config", "nvim-compe")
 time([[Config for nvim-compe]], false)
+-- Config for: telescope.nvim
+time([[Config for telescope.nvim]], true)
+try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\vconfig\22plugins.telescope\frequire\0", "config", "telescope.nvim")
+time([[Config for telescope.nvim]], false)
+-- Config for: diffview.nvim
+time([[Config for diffview.nvim]], true)
+try_loadstring("\27LJ\2\n3\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\24plugins.gitdiffview\frequire\0", "config", "diffview.nvim")
+time([[Config for diffview.nvim]], false)
 -- Config for: nvim-treesitter
 time([[Config for nvim-treesitter]], true)
 try_loadstring("\27LJ\2\nA\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\vconfig\23plugins.treesitter\frequire\0", "config", "nvim-treesitter")
@@ -182,14 +190,6 @@ time([[Config for nvim-treesitter]], false)
 time([[Config for nvim-autopairs]], true)
 try_loadstring("\27LJ\2\n1\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\22plugins.autopairs\frequire\0", "config", "nvim-autopairs")
 time([[Config for nvim-autopairs]], false)
--- Config for: nvim-lspconfig
-time([[Config for nvim-lspconfig]], true)
-try_loadstring("\27LJ\2\n+\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\16plugins.lsp\frequire\0", "config", "nvim-lspconfig")
-time([[Config for nvim-lspconfig]], false)
--- Config for: telescope.nvim
-time([[Config for telescope.nvim]], true)
-try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\vconfig\22plugins.telescope\frequire\0", "config", "telescope.nvim")
-time([[Config for telescope.nvim]], false)
 vim.cmd [[augroup packer_load_aucmds]]
 vim.cmd [[au!]]
   -- Event lazy-loads
