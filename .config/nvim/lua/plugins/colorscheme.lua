063b92600316f541c0692ddc2d578ade460bca04
gcalmettes <guillaume.calmettes@gmail.com>
2024-05-17 15:47:54 +0200

    enh: v0.10 has automatic truecolor detection

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -3,10 +3,6 @@ return {
         "mhartington/oceanic-next",
         opts = {},
         config = function(_, opts)
-          -- True color support
-          if vim.fn.has("termguicolors") == 1  then
-              vim.opt.termguicolors = true
-          end
           require("utils").SetupOceanicNext()
         end
     },
