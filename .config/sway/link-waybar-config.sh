f30974edcfcc6b86f998d0dece6f92bd64ee2d09
Guillaume Calmettes <gcalmettes@scaleway.com>
2024-12-01 15:29:26 +0100

    feat: use sway waybar config if session is not an hyprland session

---
+++ .config/sway/link-waybar-config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# link up sway waybar config
+ln -sf "$HOME/.config/waybar/config.d/sway-style.css" "$HOME/.config/waybar/style.css"
+ln -sf "$HOME/.config/waybar/config.d/sway-config.jsonc" "$HOME/.config/waybar/config.jsonc"
